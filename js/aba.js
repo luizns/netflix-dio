d9b2083cb0852d80d0fe3ca0fe16d5f8e6c1fedd
Luiz Nascimento <Luiz Nascimento>
2021-03-08 15:39:10 -0300

    Projeto Inicial

---
+++ js/aba.js
@@ -0,0 +1,10 @@
+const click1 = document.getElementById('bt1')
+
+click1.addEventListener('click', () => {
+    window.open("trailer.html")
+})
+const click2 = document.getElementById('bt2')
+
+click2.addEventListener('click', () => {
+    window.open("https://www.netflix.com/br/title/70195800")
+})
